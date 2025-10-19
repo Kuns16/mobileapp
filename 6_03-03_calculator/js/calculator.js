06010cdd68ce164bdb7af3283e524ee19fe7e23e
Kuns16 <kuns16042003@gmail.com>
2025-10-20 01:07:36 +0900

    Add files via upload

---
+++ 6_03-03_calculator/js/calculator.js
@@ -0,0 +1,43 @@
+'use strict';
+//
+const elementSelect = document.getElementById("calcType");
+const elementNum1 = document.getElementById("num1");
+const elementNum2 = document.getElementById("num2");
+const elementResult = document.getElementById("result");
+
+elementSelect.addEventListener("change", update);
+elementNum1.addEventListener("change", update);
+elementNum2.addEventListener("change", update);
+//関数「update」を作り、処理をまとめる
+function update(){
+    //計算結果を求める
+    const result = calculate(
+        Number(elementNum1.value),
+        Number(elementNum2.value),
+        elementSelect.value
+    );
+
+    //画面に表示
+    elementResult.innerHTML = result;
+}
+
+//
+function calculate(num1, num2, calcType){
+    let result;
+        //
+        switch(calcType){
+            case "type-add":
+                result = num1 + num2;
+                break;
+            case "type-substract":
+                result = num1 - num2;
+                break;
+            case "type-multiply":
+                result = num1 * num2;
+                break;
+            case "type-divide":
+                result = num1 / num2;
+                break;
+        }
+    return result;
+}
